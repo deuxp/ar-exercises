ca0645b648e3d7460425539bd655f53de8738da8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-06 10:28:58 -0400

    destroy store 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+#
+# This activity is to Delete the 3rd Store from db
+# We will accomplish this by useing the `destroy` method
+
+p "\t This is store 3 #{Store.find(3).name}"
+@store3.destroy
+# the following throws an error since the Store is delete 
+# p "\t This is store 3 #{Store.find(3).name}"
