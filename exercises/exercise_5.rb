522d9c1368b9f592cad2c0e74f12b1b86b8ce4a6
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-06 13:49:48 -0400

    implement calcuations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+total_stores = Store.count
+over_one_mil = Store.where('annual_revenue > 1000000').count
+puts "The total revenue for all the stores is #{total_revenue}"
+puts "The average revenue for all stores is #{total_revenue / total_stores}"
+puts "There are #{over_one_mil} stores that are making more than one million."
+
