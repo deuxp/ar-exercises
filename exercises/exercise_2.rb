d6878366d1a910d91ea24700495b16029d7c8a4f
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-06 12:08:21 -0400

    add @store3 by id

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,9 @@ puts "----------"
 
 # Your code goes here ...
 
-@store1 = Store.first
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store3 = Store.find(3)
 p "this is store 1 from the db: #{@store1.name}"
 @store1.name = 'Thornhill'
 p "this is store 1 from the db: #{@store1.name}"
