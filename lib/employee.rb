07c7fa6b652d814281c6be7acc1993dd150210fb
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-07 12:03:31 -0400

    add validators to the Employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
-  validates_presence_of :first_name, :last_name
-  validates_numericality_of :hourly_rate, greater_than_or_equal_to: 40, less_than_or_equal_to: 200
-  # :store or :store_id
-   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_inclusion_of :hourly_rate, :in => 40..200
+  validates :store_id, :presence => true
 end
